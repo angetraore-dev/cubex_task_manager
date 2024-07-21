ef4fa1dfc49909ec4a5fc68ae08daa000cafac52
angetraore-dev <sonia.traore@ymail.com>
2024-07-21 22:34:05 +0000

    first line button and add task client side done

--- templates/admin/index.php
+++ templates/admin/index.php
@@ -4,6 +4,7 @@
     <!-- Container for first buttons line -->
     <div class="row d-flex justify-content-between">
         <div class="col-md-7 g-3 border-2 border-danger">
+            <!-- add department delete department-->
             <div class="justify-content-around py-2">
                 <button type="button" class="col-4 btn btn-success btn-outline-light text-uppercase border-success">
                     <span class="fw-bold">
@@ -41,11 +42,12 @@
             </div>
         </div>
 
-        <div class="col-md-4 border-2 border-primary">
+        <!-- filter for departments and responsibles -->
+        <div class="col-md-4 g-3 border-2 border-primary">
            <div class="justify-content-around py-2 d-inline-flex">
 
-               <div class="dropdown">
-                   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+               <div class="dropdown mx-2">
+                   <button class="btn btn-outline-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                        departments
                    </button>
                    <ul class="dropdown-menu">
@@ -57,8 +59,8 @@
 
 
 
-               <div class="dropdown">
-                   <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+               <div class="dropdown mx-2">
+                   <button class="btn btn-outline-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                        responsibles
                    </button>
                    <ul class="dropdown-menu">
@@ -76,9 +78,11 @@
 
     <!-- Container for add a Task -->
     <div class="row d-flex justify-content-center align-items-center my-4">
-        <div class="col-sm-4">
-            <button type="button" class="btn btn-success btn-outline-light">Add Task</button>
-        </div>
+        <button type="button" class="btn btn-success btn-outline-light col-4 text-uppercase">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 24 24">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
+            </svg>Add Task
+        </button>
     </div>
 
 </div>
